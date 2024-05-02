e4472b6e632b211ac314aa9733bb998c15799fca
Mario Contreras <mariocc29@gmail.com>
2024-05-02 06:50:57 -0400

    added DeckId to serializer

--- app/api/v1/serializers/deck_serializer.rb
+++ app/api/v1/serializers/deck_serializer.rb
@@ -4,6 +4,7 @@ module V1
   module Serializers
     # Defines the structure for serializing deck information.
     class DeckSerializer < Grape::Entity
+      expose :id, documentation: { type: Integer, desc: 'Identifier of the deck.' }
       expose :label, documentation: { type: String, desc: 'Label or name of the deck.' }
       expose :category, documentation: { type: String, desc: 'The types of Pokémon included in the deck.' }
       expose :cards, documentation: { type: Array, desc: 'Array of cards in the deck.' } do |deck, options|
